0188be8c77465026f36b78b2bac6b0ef42d7ae82
Your Name <Sanchit Pandey>
2024-03-23 22:49:12 +0545

    catching all segments(...slugs)

---
+++ src/app/docs/[...slug]/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Docs = ({
+  params,
+}: {
+  params: {
+    slug: string[];
+  };
+}) => {
+    if (params.slug.length===2){
+  return <h1>
+    Viewing all docs for {params.slug[0]} and {params.slug[1]} 
+    </h1>;
+} else if(params.slug.length===1) {
+
+return <h1>Viewing docs for feature  {params.slug[0]}</h1>
+}
+};
+
+export default Docs;
