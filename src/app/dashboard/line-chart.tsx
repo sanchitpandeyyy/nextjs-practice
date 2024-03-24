a5f32b93f96892cc0e1464d684c045aa47b5203b
Your Name <Sanchit Pandey>
2024-03-25 01:50:50 +0545

    file colocation

---
+++ src/app/dashboard/line-chart.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LineChart = () => {
+  return (
+    <div>LineChart hello</div>
+  )
+}
+
+export default LineChart;
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+function barChart(){
+    return <h1>barchart</h1>
+}
+
+import React from 'react'
+
+const Dashboard= () => {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
