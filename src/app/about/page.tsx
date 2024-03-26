82a0c0b309ceae695886a23b0a9c9367c9d0fa54
Your Name <Sanchit Pandey>
2024-03-27 00:38:27 +0545

    added dunamic Routing metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+    title: "About section"
+};
+
 export default function About(){
     return <h1>About</h1>
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import { Metadata } from "next";
 
-const ProductDetails = ({ 
-    params
-  }: { 
-    params: { productId: String } 
-   
-  }) => {
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  const title = await new Promise(resolve=>{
+    setTimeout(()=>{
+      resolve(`Iphone ${params.productId}`)
+    },4000)
+  })
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+const ProductDetails = ({ params }: { params: { productId: String } }) => {
   return <div>Details of Prod {params.productId}</div>;
 };
 
